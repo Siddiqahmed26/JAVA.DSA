604c789bd67776aacd35fed67bdaf9624cec18d8
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2023-09-23 03:25:00 +0530

    basics

---
+++ variables/input.java
@@ -0,0 +1,19 @@
+
+    import java.util.*;
+
+public class input{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        String name=sc.nextLine();
+         System.out.println(name);
+
+        int number= sc.nextInt();
+        System.out.println(number);
+
+         float num= sc.nextFloat();
+        System.out.println(num);
+
+
+    }
+    
+}
---
+++ variables/variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    public static void main(String args[]){
+        int a = 10; 
+        int b = 6;
+         System.out.println(b);
+                System.out.println(a);
+            }
+    
+}
