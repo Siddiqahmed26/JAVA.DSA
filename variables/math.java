1edfb88791c50b43f4ffe74c7dfe5463a51f7452
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2023-09-27 00:09:09 +0530

    basics

--- variables/math.java
+++ variables/math.java
@@ -1,16 +1,29 @@
 import java.util.*;
-public class sum1 {
+public class math {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+
+        // addition
         int a = sc.nextInt();
         int b = sc.nextInt();
+         System.out.println("Additon is :");
         int sum = a+b;
         System.out.println(sum);
+
+        //multiplication
 
             int c = sc.nextInt();
         int d = sc.nextInt();
         int mul = c*d;
+          System.out.println("Multiplication is :");
         System.out.println(mul);
+
+        // division
+        float x = sc.nextFloat();
+        float y = sc.nextFloat();
+        float div = x/y;
+          System.out.println("Division is :");
+        System.out.println(div);
     }
     
 }
