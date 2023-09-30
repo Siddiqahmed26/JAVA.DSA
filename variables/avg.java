76ea1951ddad750491ad750518ff045a5d4762cf
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2023-10-01 01:07:28 +0530

    basics

---
+++ variables/avg.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class avg {
+ public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+     System.out.println("enter 3 numbers to find the average");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int avg = (a+b+c)/3;
+     System.out.println("the average is :");
+     System.out.println(avg);
+
+
+ }   
+}
