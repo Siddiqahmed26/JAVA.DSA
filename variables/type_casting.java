552ca0322abab5b22da28540b0180a2cd4008f11
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2023-09-30 23:56:56 +0530

    basics

--- variables/type_casting.java
+++ variables/type_casting.java
@@ -3,8 +3,12 @@ public class type_casting{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         char ch='a';
+        char ch1 = 'b';
+        int num2= ch1;
         int num= ch;
         System.out.println(num);
+        System.out.println(num2);
+        
 
 
     }
