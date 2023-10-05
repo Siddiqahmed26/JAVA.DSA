f34e560348e95203fecf81c607f56e3ebc6ab89d
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2023-10-05 20:56:46 +0530

    basics

---
+++ statements/switchZ.java
@@ -0,0 +1,26 @@
+    import java.util.*;
+public class switchZ {
+    public static void main(String args[]){
+     
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your choice btw 1-3");
+        int number = sc.nextInt();
+       
+    
+        switch(number){
+            case 1: System.out.println("buger");
+            break;
+            case 2: System.out.println("omlet");
+            break;
+            case 3: System.out.println("Fruits");
+        break;
+        default: System.out.println("wake up");
+        }
+
+    
+    }
+    
+}
+
+    
+
