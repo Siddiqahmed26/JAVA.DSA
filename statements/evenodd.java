b0257e14487ff1962c6d42f7b560c06000c293e2
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2023-10-05 19:21:04 +0530

    basics

--- statements/evenodd.java
+++ statements/evenodd.java
@@ -2,9 +2,9 @@
     import java.util.*;
 public class evenodd {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter number");
-        int n = sc.nextInt();
+         Scanner sc = new Scanner(System.in);
+         System.out.println("enter number");
+         int n = sc.nextInt();
         if(n%2==0){
             System.out.println("even");
 
@@ -12,6 +12,11 @@ public class evenodd {
         else{
             System.out.println("odd");
         }
-    
+
+        //USING TERNARY OPERTATOR
+     
+        String type = (n%2==0)? "even":"odd";
+        System.out.println(type);
 }
+
 }
