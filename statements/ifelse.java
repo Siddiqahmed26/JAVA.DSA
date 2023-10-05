40a9793e798027c4910cbb48a6c6a36cd10df32c
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2023-10-05 14:36:29 +0530

    basics

---
+++ statements/ifelse.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ifelse{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter age");
+        int n = sc.nextInt();
+        
+        if(n>18){
+            System.out.println("Vote");
+
+        
+        if(n>12 && n<18){
+            System.out.println("Teenager");
+    
+        }
+    }
+else{
+    System.out.println("not adult");
+}
+    }
+
+}
