f9c2ef34adaeacfd33a8428fd4aabf58c0a5616f
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2023-10-11 21:25:09 +0530

    basics

---
+++ loops/x100.java
@@ -0,0 +1,11 @@
+public class x100 {
+    public static void main(String args[]){
+        int counter =0;
+        while(counter<100){
+            System.out.println("hello world");
+            counter++;
+        }
+        System.out.println("printer hello world 100x");
+    }
+    
+}
