70208af4b3a16064fceb96d1d373d6d735314be8
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2024-03-29 16:17:07 +0530

    .

---
+++ methods/Comtest.java
@@ -0,0 +1,45 @@
+class Product {
+    String name;
+    double price;
+    Company c;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    void buy(Company c) {
+        System.out.println(this.name + "is a product");
+        c.cInfo();
+
+    }
+}
+class Company {
+    String name;
+    int stocks;
+
+    Company(String name, int stocks) {
+        this.name = name;
+        this.stocks = stocks;
+
+    }
+
+    void cInfo() {
+        System.out.println("company info");
+        System.out.println("company name" + this.name);
+        System.out.println("company stocks" + this.stocks);
+    }
+
+}
+
+public class Comtest {
+    public static void main(String[] args) {
+
+        Company c = new Company("Amazon", 10);
+
+        Product p = new Product("Mobile", 14000);
+
+        p.buy(c);
+
+    }
+}
