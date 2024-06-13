99e73bf811abc0e100c8969a51b6949c2d80b0d2
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2024-06-13 18:53:42 +0530

    .

---
+++ methods/Calc.java
@@ -0,0 +1,18 @@
+public class Calc{
+  void add()
+    {
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println(c);
+    }
+
+    public static void main(String[] args){
+        Calc c = new Calc();
+        c.add();
+
+    
+
+}
+}
