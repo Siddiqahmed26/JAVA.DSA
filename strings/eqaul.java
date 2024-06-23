6c180cad40553c1ef340d544679fb818a3c6a106
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2024-06-24 00:58:37 +0530

    .

---
+++ strings/eqaul.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class eqaul {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2= "bello";
+
+        if(s1.equals(s2)){
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+    
+}
