cf2ed589b6e772aa7ad9f4db6f8843fe24aad228
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2024-06-25 06:15:03 +0530

    .

---
+++ variables/ternary.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ternary{
+    public static void main(String [] args){
+       
+
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+
+        
+        
+
+        String result= (marks>=33)? "pass": "fail";
+        System.out.println(result);
+      
+    }
+}
