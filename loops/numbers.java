a6b6663e231d45c26cbef0ed1548dfc1305f3e78
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2024-06-25 06:42:15 +0530

    .

--- loops/numbers.java
+++ loops/numbers.java
@@ -1,8 +1,11 @@
+import java.util.*;
 public class numbers {
     public static void main(String args[]){
-        int counter = 1;
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+         int counter=1;
         // printing numbers from 1-1000
-        while(counter<=1000){
+        while(counter<=n){
             System.out.print(counter+"  ");
             counter++;
 
