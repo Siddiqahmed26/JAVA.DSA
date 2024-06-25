b49953ffc6f62b086c4e27e7e7a2261c6060ad00
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2024-06-25 06:27:15 +0530

    .\

---
+++ loops/for1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class for1 {
+    public static void main(String[] args) {
+int i;
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            for (i=1; i<n; i++) {
+                System.out.println("hello world");
+            }
+
+    
+}
+}
