25e6b790429e6db6ff40813dbf4583612b5349bc
siddiqahmed26 <siddiqahmed.20is@saividya.ac.in>
2024-06-27 05:21:32 +0530

    .

---
+++ loops/reverse.java
@@ -0,0 +1,43 @@
+// public class reverse {
+//     public static void main(String[] args) {
+       
+//       for (int n=1234; n>0; n=n/10){
+//         System.out.print(n%10);
+
+
+
+//       }
+//     }
+// }
+
+// public class reverse {
+//         public static void main(String[] args) {
+
+//             int n =98948;
+//             while(n>0){
+//                int lastdigit=n%10;
+//                System.out.print(lastdigit+" "); 
+//                n=n/10;
+//             }
+//         }
+//     }
+           
+
+
+public class reverse {
+        public static void main(String[] args) {
+
+            int n =2345643;
+            int rev=0;
+            while(n>0){
+               int lastdigit=n%10;
+               rev = (rev*10)+lastdigit;
+               n=n/10;
+               
+               
+            }
+            System.out.print(rev); 
+               
+        }
+    }
+           
